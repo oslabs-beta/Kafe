1ea37aff59022911d02c35834aadcc2df8d44d6a
Jacob Cole <jacob.cole@gmail.com>
2022-11-26 15:38:40 -0500

    Imports and boilerplate query

--- src/client/components/Brokers.tsx
+++ src/client/components/Brokers.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { useQuery } from "@apollo/client";
+
+const brokerQuery =   gql`
+query GetCluster {
+  cluster {
+    brokers {
+      id
+      host
+      port
+    }
+  }
+}
+`
 
 function Brokers(){
 
--- src/client/components/Partitions.tsx
+++ src/client/components/Partitions.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { useQuery } from "@apollo/client";
+
+const partitionQuery =   gql`
+query GetCluster {
+  cluster {
+    brokers {
+      id
+      host
+      port
+    }
+  }
+}
+`
 
 function Partitions(){
 
