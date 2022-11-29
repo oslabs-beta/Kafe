c02a3231e0c6094811373a3bd129e1e7d7e4111b
Jacob Cole <jacob.cole@gmail.com>
2022-11-28 23:07:08 -0500

    Group work on ChartJS

--- src/client/components/BarChart.tsx
+++ src/client/components/BarChart.tsx
@@ -42,24 +42,24 @@ const chartData = [
 console.log(chartData);
 
 function BarChart(chartData){
-    const [userData, setUserData] = useState({
-        labels: chartData?.map((data) => data.year),
-        datasets: [
-          {
-            label: "Users Gained",
-            data: chartData?.map((data) => data.userGain),
-            backgroundColor: [
-              "rgba(75,192,192,1)",
-              "#ecf0f1",
-              "#50AF95",
-              "#f3ba2f",
-              "#2a71d0",
-            ],
-            borderColor: "black",
-            borderWidth: 2,
-          },
-        ],
-      });
+    // const [userData, setUserData] = useState({
+    //     labels: chartData?.map((data) => data.year),
+    //     datasets: [
+    //       {
+    //         label: "Users Gained",
+    //         data: chartData?.map((data) => data.userGain),
+    //         backgroundColor: [
+    //           "rgba(75,192,192,1)",
+    //           "#ecf0f1",
+    //           "#50AF95",
+    //           "#f3ba2f",
+    //           "#2a71d0",
+    //         ],
+    //         borderColor: "black",
+    //         borderWidth: 2,
+    //       },
+    //     ],
+    //   });
     
 return(
 <Bar data = {{
