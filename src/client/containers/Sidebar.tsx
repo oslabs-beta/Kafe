7e2d1218cea22df04a91c50f9cec489123e5adf2
Jacob Cole <jacob.cole@gmail.com>
2023-01-03 12:35:24 -0500

    Status component in progress

--- src/client/containers/Sidebar.tsx
+++ src/client/containers/Sidebar.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Status from '../components/Status';
+import React, { useState, useEffect } from 'react';
+import { useQuery } from "@apollo/client";
 import Menu from '../components/Menu';
+import { CLUSTER_SUMMARY } from '../queries/graphQL'; 
 // For use with logo and icons; yet to be implemented
 import { SvgIcon } from '@mui/material';
 
@@ -12,10 +13,22 @@ const divStyle = {
 
 function Sidebar(){
 
+    let connectStatus = 'Loading';
+    const status = useQuery(CLUSTER_SUMMARY);
+    console.log('   status is... ', status);
+    if(status.data)connectStatus = 'Connected';
+
+    // check session if dashboard remounts
+
+//   useEffect(() => {
+//     verifyConnection();
+//   }, []); //Run this once at the beginning
+
+
 return(
     <>
+        <div>System Status<br />{connectStatus}</div>
         <div style={divStyle}>
-            <Status />
             <Menu />
         </div>
     </>
