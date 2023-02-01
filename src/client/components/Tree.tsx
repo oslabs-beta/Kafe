201a938c39b10dd992b33624f677d482f4b0e05b
zezang <zezang@gmail.com>
2023-01-31 20:46:15 -0800

    Implemented centering of tree

--- src/client/components/Tree.tsx
+++ src/client/components/Tree.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, useRef, Suspense } from 'react';
-import { useQuery } from "@apollo/client";
-import { TREE_DATA } from '../queries/graphQL';
+import { RawNodeDatum } from 'react-d3-tree/lib/types/types/common';
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Tree from 'react-d3-tree';
-import { RawNodeDatum } from 'react-d3-tree/lib/types/types/common';
+import { useQuery } from "@apollo/client";
+import { TREE_DATA } from '../queries/graphQL';
+import { useCenteredTree } from '../utils/treeHelper';
 
 
 //attributes rendered as a list of secondary labels
 function KafkaTree() {
   const [treeData, setTreeData] = useState <RawNodeDatum | RawNodeDatum[]>({name: 'Cluster', children: []});
+  const [translate, containerRef] = useCenteredTree();
   const { loading, data } = useQuery(TREE_DATA);
 
   useEffect(()=> {
@@ -40,7 +42,6 @@ function KafkaTree() {
 
       for (const partition of topic.partitions) {
         const { leader } = partition;
-        console.log('Partition leader', leader);
 
         treeData.children[leader - 1].children.push({
           name: `Partition ${partition.partitionId}`,
@@ -57,8 +58,12 @@ function KafkaTree() {
   return (
     <>
        <Container maxWidth="xl" sx={{ mt: 2, mb: 4 , height: '100vh'}}>
-        <Grid container spacing={3} sx={{height: '100%'}}>
-          <Tree data = {treeData} orientation = "vertical"/>
+        <Grid container spacing={3} sx={{height: '100%'}} ref={containerRef}>
+          <Tree 
+            data = {treeData} 
+            orientation = "vertical"
+            translate={translate}
+            />
         </Grid>
        </Container>
     </>
